7794c5fae296dea7acdb4098dc30d8fb24afc60c
Nwabueze Christian Izuchukwu <izuchukwujnwabueze@gmail.com>
2025-04-18 04:44:04 -0700

    Update and rename components/Card.js to components/ui/card.js

--- components/Card.js
+++
@@ -1,7 +0,0 @@
-export default function Card({ children, className }) {
-  return (
-    <div className={`p-4 shadow-lg rounded-lg ${className || ""}`}>
-      {children}
-    </div>
-  );
-}
---
+++ components/ui/card.js
@@ -0,0 +1,7 @@
+export function Card({ children }) {
+  return (
+    <div className="p-4 border rounded shadow bg-white">
+      {children}
+    </div>
+  );
+}
