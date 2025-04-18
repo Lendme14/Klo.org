0bd5c4ce60f465400def80f4af42c15b01784c3f
Nwabueze Christian Izuchukwu <izuchukwujnwabueze@gmail.com>
2025-04-18 04:42:40 -0700

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,10 +1,8 @@
-export default function Button({ children, onClick, className }) {
+export function Button({ children, ...props }) {
   return (
-    <button
-      onClick={onClick}
-      className={`px-4 py-2 rounded ${className || ""}`}
-    >
+    <button {...props} className="px-4 py-2 bg-blue-600 text-white rounded">
       {children}
     </button>
   );
 }
+
